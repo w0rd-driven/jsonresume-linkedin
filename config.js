49ce368f007afc949e98e0c350f28cf21922c2f1
Jeremy Brayton <jeremy@braytonium.com>
2015-09-05 19:58:46 -0400

    Basic configuration created. Need to populate with testing data to match the dummy resume

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  api_url: "https://api.linkedin.com/v1/",
+  api_format: 'json',
+  api_fields: [
+    'id', 'formatted-name', 'headline', 'picture-url', 'email-address', 'phone-numbers', 'summary', 'main-address',
+    'public-profile-url', 'primary-twitter-account', 'positions', 'projects', 'volunteer', 'educations', 'certifications',
+    'honors-awards', 'publications', 'skills', 'languages', 'interests', 'recommendations-received'
+  ],
+  data: {
+
+  }
+};
